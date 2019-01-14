c3d03d664c15d33562472cb80095d995fce2f271
Jannie <jan.vijfhuizen@outlook.com>
2019-01-14 17:03:46 +0000

    Singletonmaterial hotfix

--- Assets/[Source]/Scripts/Core/Utilities/SingletonMaterial.cs
+++ Assets/[Source]/Scripts/Core/Utilities/SingletonMaterial.cs
@@ -8,8 +8,6 @@ public class SingletonMaterial : MonoBehaviour
     private MaterialPropertyBlock propBlock;
     private Coroutine lerpColorTo;
 
-    public AnimationCurve curve;
-
     private void Awake()
     {
         propBlock = new MaterialPropertyBlock();
